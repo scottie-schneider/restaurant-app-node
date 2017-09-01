5f50ac58a94b085bcf53bf04b671b302ca291b5e
Scottie Schneider <scott@laclave.co>
2017-09-01 14:32:50 -0500

    Added 'hello.pug', testing routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'wes'};
+  //res.json(wes);
+  //res.send('Hey! It works!');
+  //res.send(req.query.name);
+  res.render('hello');
 });
 
 module.exports = router;
