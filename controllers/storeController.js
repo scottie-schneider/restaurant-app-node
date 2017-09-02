6a01fa362300c6a12c60e7b5d4cacddc43126041
Scottie Schneider <scott@laclave.co>
2017-09-02 12:39:48 -0500

    Update 2 ✅ Used storeController.js, async await, and mongoose to save data to DB

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.myMiddleware = (req, res, next) => {
 	req.name = 'Wes';
 	next();
@@ -7,4 +10,10 @@ exports.homePage = (req, res) => {
 } 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: ' Add Store' });
+}
+
+exports.createStore = async (req, res) => {
+	const store = new Store(req.body);
+	await store.save();
+	res.redirect('/');
 }
